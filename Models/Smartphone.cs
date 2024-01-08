e68b3cb6aa79d0de64b07a73bf765b377eb73159
madu5 <MM218@outlook.com.br>
2024-01-08 17:37:06 -0300

    requisitos do desafio concluídos

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,22 +3,32 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        // ✅ Implementar as propriedades faltantes de acordo com o diagrama
+        private string _modelo, _imei;
+        private int _memoria;
 
-        public Smartphone(string numero)
+        // Não podem ser alterados depois do objeto ser criado, só podem ser lidos
+        public int Memoria { get => _memoria; }
+        public string Imei { get => _imei; }
+        public string Modelo { get => _modelo; }
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            // ✅ Passar os parâmetros do construtor para as propriedades
+            _modelo = modelo;
+            _imei = imei;
+            _memoria = memoria;
         }
 
-        public void Ligar()
+        public void Ligar(string numero)
         {
-            Console.WriteLine("Ligando...");
+            Console.WriteLine($"Ligando para {numero}...");
         }
 
         public void ReceberLigacao()
         {
-            Console.WriteLine("Recebendo ligação...");
+            Console.WriteLine($"Recebendo ligação...");
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
--- Program.cs
+++ Program.cs
@@ -1,3 +1,27 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// ✅ Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Testando Nokia...");
+Smartphone nokia = new Nokia(numero: "987824591", modelo: "Nokia 3310", imei: "123456789", memoria: 4);
+nokia.Ligar("987654321");
+nokia.InstalarAplicativo("Snake");
+nokia.ReceberLigacao();
+Console.WriteLine("\nInformações do Nokia:\n" +
+    $"Número: {nokia.Numero}\n" +
+    $"Modelo: {nokia.Modelo}\n" +
+    $"IMEI: {nokia.Imei}\n" +
+    $"Memória: {nokia.Memoria}GB\n");
+
+Console.WriteLine("--------------------------------");
+
+Console.WriteLine("\nTestando iPhone...");
+Smartphone iphone = new Iphone(numero: "987654321", modelo: "iPhone 12", imei: "103050709", memoria: 128);
+iphone.Ligar("987824591");
+iphone.InstalarAplicativo("Instagram");
+iphone.ReceberLigacao();
+Console.WriteLine("\nInformações do iPhone:\n" +
+    $"Número: {iphone.Numero}\n" +
+    $"Modelo: {iphone.Modelo}\n" +
+    $"IMEI: {iphone.Imei}\n" +
+    $"Memória: {iphone.Memoria}GB");
